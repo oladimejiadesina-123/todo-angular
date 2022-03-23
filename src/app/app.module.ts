9b415f6a961b290b686fe8b750e654cb982d771b
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-03-23 22:52:48 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { TodoComponent } from './todo/todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
